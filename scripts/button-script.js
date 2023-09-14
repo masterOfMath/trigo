ee67dc7e36d4603c40bb2f841d2abb2662ea597d
dimasnovik <127199723+dimasnovik@users.noreply.github.com>
2023-09-14 11:29:33 +0400

    tabs added

--- scripts/button-script.js
+++ scripts/button-script.js
@@ -33,10 +33,13 @@ var input = document.getElementById('toggleswitch');
     })
 
     const taskBtn = btns.item(16); 
+    let n = 0;
     taskBtn .onclick = async function () {
       allBtnsToWhite();
-      document.querySelector(".sin").style.visibility = "hidden";
-      document.querySelector(".cos").style.visibility = "hidden";
+      // document.querySelector(".sin").textContent ="";
+      // document.querySelector(".cos").textContent ="";
+      // document.querySelector(".sin").style.visibility = "hidden";
+      // document.querySelector(".cos").style.visibility = "hidden";
       document.querySelector(".randomEq").style.visibility = "visible";
       document.querySelector(".show").disabled = false;
       document.querySelector(".check").disabled = false;
@@ -45,6 +48,50 @@ var input = document.getElementById('toggleswitch');
       
       textSin.textContent = eq.text;
       await MathJax.typeset();
+      let clickedDots = [];
+      for (let i = 0; i < 16; i++) {
+  
+        const btn = btns.item(i);
+        btn .onclick = async function () {
+          if (btn.style.backgroundColor === "red") {
+            btn.style.backgroundColor = "white";
+            if (clickedDots.length === 1 && clickedDots[0]===i) {
+                clickedDots.pop();
+            } else {
+              const index = clickedDots.valueOf(i);
+            clickedDots.splice(index,1);
+            }
+
+          } else {
+            btn.style.backgroundColor = "red";
+            clickedDots.push(i);
+          }   
+          console.log(clickedDots);
+      }
+      }
+      checkBtn = document.querySelector(".check");
+      
+      checkBtn.onclick = async function () {
+        console.log("check", eq.dots,clickedDots);
+
+          if (arraysEqual(eq.dots,clickedDots)) {
+            document.querySelector(".progress").textContent =(++n) +" подряд!";
+            document.querySelector(".progress").style.color = "green";
+            clickedDots.forEach(x => btns.item(x).style.backgroundColor = "green");
+
+            clickedDots.forEach(x => btns.item(x).style.backgroundColor = "white");
+            clickedDots.pop();
+            clickedDots.pop();
+          } else {
+            document.querySelector(".progress").textContent = "0 подряд!";
+            document.querySelector(".progress").style.color = "red";
+            clickedDots.forEach(x => btns.item(x).style.backgroundColor = "white");
+            clickedDots.pop();
+            clickedDots.pop();
+
+            n = 0;
+          }
+      }
       const showBtn = btns.item(18);
       showBtn.onclick = function(){
         for (let i = 0; i < eq.dots.length; i++) {
@@ -70,7 +117,7 @@ for (let i = 0; i < 16; i++) {
     btns.item(i).style.backgroundColor = "red";
       
 }
-
+}
 
 
 function allBtnsToWhite() {
@@ -78,4 +125,8 @@ function allBtnsToWhite() {
       btns.item(i).style.backgroundColor = "white";
   }
 }
-};
+function arraysEqual(a1,a2) {
+  a1.sort();
+  a2.sort();
+  return JSON.stringify(a1)==JSON.stringify(a2);
+}
